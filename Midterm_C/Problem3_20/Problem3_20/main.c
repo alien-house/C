958cec81ba65a314bb8440a8146c47473e5d03cf
alien-house <info@alien-house.com>
2017-01-30 18:21:57 -0800

    midterm

--- Midterm_C/Problem3_20/Problem3_20/main.c
+++ Midterm_C/Problem3_20/Problem3_20/main.c
@@ -12,6 +12,13 @@ int main(int argc, const char * argv[]) {
     int a[3] = {1, 2, 3};
     int *p = a;
     int **r = &p;
+    printf("%p \n", a);
+    printf("%d \n", *p);
+    printf("%p \n", p);
+    printf("%d \n", **r);
+    printf("%p \n", &p);
+    printf("%p \n", p);
+    printf("%p \n", r);
     printf("%p %p", *r, a);
     return 0;
 }
